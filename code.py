1fa3b07daf71cc064f345b363707c7520e327cb5
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2023-06-07 14:49:48 +0200

    test

--- code.py
+++ code.py
@@ -7,7 +7,21 @@ from gazebo_msgs.srv import GetModelState
 from sensor_msgs.msg import LaserScan
 
 def SLAM():
-    return
+    rospy.init_node('slam_node', anonymous=True)
+    
+    # Create the publishers and other required variables
+    
+    # Move the robot forward
+    twist_msg = Twist()
+    twist_msg.linear.x = 0.1
+    vel_pub.publish(twist_msg)
+    
+    # Wait for a small duration
+    rospy.sleep(1.0)
+    
+    # Stop the robot
+    twist_msg.linear.x = 0.0
+    vel_pub.publish(twist_msg)
 
 if __name__ == '__main__':
     try:
