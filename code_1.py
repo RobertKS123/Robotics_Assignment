33ad17e9cda5efbe6b41f289790615ff7c3c11a3
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2023-06-07 21:04:22 +0200

    changed to 10

--- code_1.py
+++ code_1.py
@@ -17,8 +17,8 @@ def move_robot():
     velocity_msg = Twist()
 
     # Set the linear and angular velocities
-    velocity_msg.linear.x = 1   # Adjust the linear velocity as needed
-    velocity_msg.angular.z = 1  # Adjust the angular velocity as needed
+    velocity_msg.linear.x = 10   # Adjust the linear velocity as needed
+    velocity_msg.angular.z = 10  # Adjust the angular velocity as needed
 
     # Keep publishing the velocity commands until Ctrl+C is pressed
     while not rospy.is_shutdown():
