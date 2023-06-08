46638f353b9d16ebd43369a8081cfea6ba387116
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2023-06-08 13:46:34 +0200

    test spin

--- code_1.py
+++ code_1.py
@@ -18,24 +18,23 @@ def move_robot():
     velocity_msg = Twist()
 
     # Set the linear and angular velocities
-    velocity_msg.linear.x = 10   # Adjust the linear velocity as needed
-    velocity_msg.angular.z = 10  # Adjust the angular velocity as needed
+    velocity_msg.linear.x = 0.15  # Adjust the linear velocity as needed
+    velocity_msg.angular.z = 0  # Adjust the angular velocity as needed
 
     # Keep publishing the velocity commands until Ctrl+C is pressed
     while not rospy.is_shutdown():
+        velocity_msg.angular.z = 0
         # Publish the velocity command
         velocity_pub.publish(velocity_msg)
         
-        # Print a message to indicate that the velocity command is published
         rospy.loginfo("Velocity command published!")
 
         # Sleep for a while to maintain the desired publishing rate
         rate.sleep()
 
         # Stop the robot after a certain time (e.g., 5 seconds)
-        rospy.sleep(5)
-        velocity_msg.linear.x = 0.0
-        velocity_msg.angular.z = 0.0
+        rospy.sleep(10)
+        velocity_msg.angular.z = 1.0
         velocity_pub.publish(velocity_msg)
 
 if __name__ == '__main__':
