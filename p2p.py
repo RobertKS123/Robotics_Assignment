2ed9d2f08277eba5da9fd87c2864da372f691a6b
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2023-06-09 21:05:03 +0200

    uggg

--- p2p.py
+++ p2p.py
@@ -26,26 +26,28 @@ def rotate_bot(current,goal):
     desired_angle = math.atan2(goal.position.y - current.position.y, goal.position.x - current.position.x)
     #desired_angle = math.atan2(current.position.y - goal.position.y, current.position.x - goal.position.x)
 
-    while abs(desired_angle - current.orientation.w) >= 0.02:
+    angle_diff = desired_angle - current.orientation.w
+
+    while abs(angle_diff) >= 0.02:
         gazebo_model_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
         current = gazebo_model_state('mobile_base', 'world').pose
 
         # Calculate the difference between the desired angle and the current angle
-        angle_diff = (desired_angle - current.orientation.w)
+        angle_diff = desired_angle - current.orientation.w
 
         # Adjust the angle difference to be within the range of -pi to pi
-        while angle_diff > math.pi:
-            angle_diff -= 2 * math.pi
-        while angle_diff < -math.pi:
-            angle_diff += 2* math.pi 
+        # while angle_diff > math.pi:
+        #     angle_diff -= 2 * math.pi
+        # while angle_diff < -math.pi:
+        #     angle_diff += 2* math.pi 
 
         # Set the angular velocity as a proportional control to the angle difference
-        kp = 0.5  # Proportional control gain
-        angular_velocity = kp * angle_diff
+        # kp = 0.5  # Proportional control gain
+        # angular_velocity = kp * angle_diff
 
         vel_msg.angular.x = 0
         vel_msg.angular.y = 0
-        vel_msg.angular.z = angular_velocity
+        vel_msg.angular.z = 0.2
 
         vel_pub.publish(vel_msg)
 
